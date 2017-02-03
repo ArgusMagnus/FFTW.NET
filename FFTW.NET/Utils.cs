6cc56c4529a1ffc8a64bdfba835a8cbe258750fb
ArgusMagnus <argusmagnus@outlook.com>
2017-02-03 15:03:03 +0100

    Use RuntimeInformation class to check if the current platform is supported

--- FFTW.NET/Utils.cs
+++ FFTW.NET/Utils.cs
@@ -7,10 +7,6 @@ namespace FFTW.NET
 {
 	static class Utils
 	{
-		static readonly bool _is64Bit = IntPtr.Size == 8;
-
-		public static bool Is64BitProcess => _is64Bit;
-
 		public static int GetTotalSize(params int[] n)
 		{
 			int result = 1;
